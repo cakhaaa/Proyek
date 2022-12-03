5af08110b5d4753a8c165a1825e64fb36c07865e
Berliano <Berliano.535190078@stu.untar.ac.id>
2022-12-03 23:19:25 +0700

    enter

--- public/modules/api/v1/routes/auth.js
+++ public/modules/api/v1/routes/auth.js
@@ -30,16 +30,16 @@ module.exports = (app) => {
           req.body.email,
           req.body.password,
         );
-
+console.log(user)
         if (!user) {
           throw new Error('Wrong email or password!');
         }
-
-        return res.json({
-          email: user.email,
-          full_name: user.full_name,
-          token: await userController.generateToken(user.id),
-        }).status(200);
+res.redirect('/dashboard/')
+        // return res.json({
+        //   email: user.email,
+        //   full_name: user.full_name,
+        //   token: await userController.generateToken(user.id),
+        // }).status(200);
       } catch (err) {
         return next(err);
       }
